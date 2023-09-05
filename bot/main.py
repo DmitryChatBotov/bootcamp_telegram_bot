85b0ad7fda546016c0ff3e3d2c65320bb33be558
starminalush <alica154323@gmail.com>
2023-09-05 14:51:21 +0500

    add voice message

--- bot/main.py
+++ bot/main.py
@@ -1,7 +1,7 @@
 import asyncio
 import logging
 
-from aiogram import Bot, Dispatcher, types
+from aiogram import Bot, Dispatcher, F, types
 from aiogram.filters.command import Command
 from bestconfig import Config
 
@@ -15,6 +15,16 @@ logging.basicConfig(level=logging.INFO)
 @dp.message(Command("start"))
 async def send_welcome_message(message: types.Message) -> None:
     await message.answer("Hello, sweetie!")
+
+
+@dp.message(F.voice)
+async def voice_message_handler(message: types.Message):
+    await message.reply_voice(message.voice.file_id)
+
+
+@dp.message(F.text)
+async def text_message_handler(message: types.Message):
+    await message.reply(message.text)
 
 
 async def main():
